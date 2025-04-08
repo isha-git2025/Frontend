ec4ca9f84d092f471e9210041715984901f37262
Chandrakant Shinde <isha.chandrakant-shinde@capgemini.com>
2025-04-08 15:17:23 +0530

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,71 @@
+// import { NgModule } from '@angular/core';
+// import { RouterModule, Routes } from '@angular/router';
+// import { DashboardComponent } from './admin/dashboard/dashboard.component';
+// import { LoginComponent } from './admin/login/login.component';
+// import { RegisterComponent } from './admin/register/register.component';
+
+// const routes: Routes = [
+//   { path: '', component: DashboardComponent },
+//   { path: 'login', component: LoginComponent },
+//   { path: 'register', component: RegisterComponent },
+
+//   {
+//     path: 'admin',
+//     loadChildren: () =>
+//       import('./admin/admin.module').then((m) => m.AdminModule),
+//   },
+//   {
+//     path: 'products',
+//     loadChildren: () =>
+//       import('./products/products.module').then((m) => m.ProductsModule),
+//   },
+//   {
+//     path: 'cards',
+//     loadChildren: () =>
+//       import('./cards/cards.module').then((m) => m.CardsModule),
+//   },
+//   {
+//     path: 'transactions',
+//     loadChildren: () =>
+//       import('./transactions/transactions.module').then((m) => m.TransactionsModule),
+//   },
+//   { path: '**', redirectTo: '', pathMatch: 'full' },
+// ];
+
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule {}
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./admin/admin.module').then((m) => m.AdminModule),
+  },
+  {
+    path: 'products',
+    loadChildren: () =>
+      import('./products/products.module').then((m) => m.ProductsModule),
+  },
+  {
+    path: 'cards',
+    loadChildren: () =>
+      import('./cards/cards.module').then((m) => m.CardsModule),
+  },
+  {
+    path: 'transactions',
+    loadChildren: () =>
+      import('./transactions/transactions.module').then((m) => m.TransactionsModule),
+  },
+  { path: '**', redirectTo: 'admin', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
